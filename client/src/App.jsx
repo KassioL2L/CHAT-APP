6898999a6b843a92e27bceb13636fbd684dd95a0
kassioL2L <kassio_leite2@hotmail.com>
2023-10-14 14:51:25 -0300

    criando a estilização do chat

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'react'
 import './App.css'
-import Join from './components/join/join'
-import Chat from './components/chat/chat'
+import Join from './components/Join/Join'
+import Chat from './components/Chat/Chat'
 
 function App() {
   const [chatVisibility, setChatVisibility] = useState(false)
-  const[socket, setSocket] = useState(null)
+  const [socket, setSocket] = useState(null)
 
   return (
-    <div className='App'>
+    <div className="App">
       {
-        chatVisibility ? <Chat socket={socket}/> : <Join setSocket={setSocket} setChatVisibility={setChatVisibility}/>
+        chatVisibility ? <Chat socket={socket} /> : <Join setSocket={setSocket} setChatVisibility={setChatVisibility} />
       }
     </div>
   )

8eea3b99b741150297b624c3275c50f4c78ecbc4
kassioL2L <kassio_leite2@hotmail.com>
2024-05-31 09:05:18 -0300

    tirando bugs

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const { Socket } = require('socket.io')
 
 const app = require('express')()
 const server = require('http').createServer(app)
-const io = require('socket.io')(server, {cors:{origin: 'http://localhost:5173'}})
+const io = require('socket.io')(server, {cors:{origin: '*'}})
 
 const PORT = 3001
 
